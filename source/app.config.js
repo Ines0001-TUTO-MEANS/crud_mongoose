38d57b10c6171a685d0fc49c8b2d2e74ad4eac89
ines0001 <none>
2018-02-19 10:14:10 +0000

    🍄📹 Checkpoint source/index.html:531956/38 source/services/users.service.js:531956/3 source/app.config.js:531956/154

--- source/app.config.js
+++ source/app.config.js
@@ -1,2 +1,7 @@
 var CrudMongoose = angular.module('CrudMongoose', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router','ngResource']);
 
+(function(app) {
+	app.controller('','','',[,function(){
+
+	}]);
+})(CrudMongoose);
