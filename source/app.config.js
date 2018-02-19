f80048098ab4c71957009d6b7afd1e2bb96f8507
ines0001 <none>
2018-02-19 10:24:28 +0000

    👢👲 Checkpoint source/app.config.js:531956/120 source/index.html:531956/33

--- source/app.config.js
+++ source/app.config.js
@@ -1,7 +1,19 @@
 var CrudMongoose = angular.module('CrudMongoose', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router','ngResource']);
 
 (function(app) {
-	app.controller('','','',[,function(){
+	app.controller('TabsCtrl',['$scope','$location',function($scope,$location){
+    $scope.selectedIndex = 0;
 
+    $scope.$watch('selectedIndex', function(current, old) {
+        switch (current) {
+            case 0:
+                $location.url("/view1");
+                break;
+            case 1:
+                $location.url("/view2");
+                break;
+            
+        }
+    
 	}]);
 })(CrudMongoose);
