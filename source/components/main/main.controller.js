54b084a6d2a491dfb148d27bd226d8e5d5b2ea57
ines0001 <none>
2018-02-20 17:04:19 +0000

    🏩🏩 Checkpoint source/index.html:531956/132 source/components/users/create/user.create.controller.js:531956/2 source/components/main/main.controller.js:531956/361

---
+++ source/components/main/main.controller.js
@@ -0,0 +1,15 @@
+/*
+MainController
+But : 
+  Centralise les contrôles et les états lié au controller principal
+Les actions:
+  Bouton Principal Creation
+  Masquer/Demasquer les onglets
+
+*/
+(function(app) {
+	app.controller('MainController', ['$scope', '$stateParams','User_factory','$mdDialog',function($scope,$stateParams,User_factory,$mdDialog) {
+    
+    
+	}]);
+})(CrudMongoose);
