3f71861f9d75eafea7a8c94e8ff3608507b0510e
Glitch (crud-mongoose) <none>
2018-02-23 14:54:26 +0000

    🐡🐏 Checkpoint app/models/Person.js:531956/81 source/components/users/list/users.list.controller.js:531956/1

--- app/models/Person.js
+++ app/models/Person.js
@@ -5,6 +5,10 @@ module.exports =  mongoose.Schema({
     type: String,
     required: 'name is mandatory'
   },
+  age: {
+    type: Number,
+    required :'age is mandatory'
+  },
   company: String,
   email: String,
   password: String
