7506ddd16e54f2158c87a403a3edef9f7ea74ae1
Glitch (crud-mongoose) <none>
2018-02-26 14:21:27 +0000

    💏🐌 Checkpoint app/models/Person.js:531956/28

--- app/models/Person.js
+++ app/models/Person.js
@@ -1,5 +1,21 @@
 // User.js
-var mongoose = require('mongoose');  
+var mongoose = require('mongoose')
+var validate = require('mongoose-validator')
+
+var nameValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [18, 99],
+    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+  }),
+  validate({
+    validator: 'isAlphanumeric',
+    passIfEmpty: true,
+    message: 'Name should contain alpha-numeric characters only',
+  }),
+]
+
+
 module.exports =  mongoose.Schema({  
   name: {
     type: String,
