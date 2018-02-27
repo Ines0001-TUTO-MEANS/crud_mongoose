507410319e3eebfe2fc0be70c14dd53ae1b6643b
Glitch (crud-mongoose) <none>
2018-02-27 15:02:10 +0000

    📒👲 Checkpoint server.js:531956/98

--- server.js
+++ server.js
@@ -41,9 +41,5 @@ app.get("/", function (request, response) {
 
 // listen for requests :)
 var listener = app.listen(port, function () {
-  console.log('url:' + listener.url());
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-//'Hello! The API is at http://localhost:' + port + '/api'
