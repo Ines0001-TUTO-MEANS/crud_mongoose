5d1b662addf3975cb4fedafe6dc1a2dde0c9c37a
Glitch (crud-mongoose) <none>
2018-03-01 23:21:14 +0000

    😭👔 Checkpoint app/routes/users.js:531956/212

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,15 +6,14 @@ var express = require('express'),
 var schema = require('../models/Person')
 var options = {
   access: function(req) {
-      if (req.isAuthenticated() && req.method.toLowerCase() === 'post') {
+      
         return 'protected';
-      } else {
-        return 'public';
-      }
+  }
+      
 }
 // Using query builder
 
-restify.serve(router, mongoose.model('Users', schema ))
+restify.serve(router, mongoose.model('Users', schema ), options)
 
 
 
