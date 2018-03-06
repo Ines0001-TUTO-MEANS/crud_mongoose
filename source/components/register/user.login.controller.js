cda12b3be6c59b214d7d23df7d34deafdd3b66ae
Glitch (crud-mongoose) <none>
2018-03-06 17:34:14 +0000

    🗃🛤 Checkpoint source/components/register/user.login.html:531956/466 source/components/error/page.html:531956/3 source/components/register/user.register.controller.js:531956/53 source/components/register/user.login.controller.js:531956/90

---
+++ source/components/register/user.login.controller.js
@@ -0,0 +1,11 @@
+(function(app) {
+	app.controller('LoginController', ['$scope', '$stateParams','User_factory','$mdDialog',function($scope,$stateParams,User_factory,$mdDialog) {
+    $scope.user={};
+    
+    $scope.login = function(){
+      
+    
+    }
+    
+	}]);
+})(CrudMongoose);
--- source/components/register/user.register.controller.js
+++
@@ -1,7 +0,0 @@
-(function(app) {
-	app.controller('RegisterController', ['$scope', '$stateParams','User_factory','$mdDialog',function($scope,$stateParams,User_factory,$mdDialog) {
-    $scope.register={};
-        
-    
-	}]);
-})(CrudMongoose);
