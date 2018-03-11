f449976e05131045099c3bdc876bfb8c232b1d5e
Glitch (crud-mongoose) <none>
2018-03-11 12:02:18 +0000

    🔫💁 Checkpoint source/components/register/user.login.controller.js:531956/243 source/components/register/user.login.html:531956/20

--- source/components/register/user.login.controller.js
+++ source/components/register/user.login.controller.js
@@ -1,13 +1,18 @@
 (function(app) {
 	app.controller('LoginUserController', ['$scope', '$state','User_factory','$mdDialog',function($scope,$state,User_factory,$mdDialog) {
     $scope.user={};
+    $scope.errorMessage = '';
     $scope.imagePath = '/img/icons/nodejs.png';
     
     $scope.login = function(form){
-      console.log(user)
-      var user_login = new User_factory(user)
+      console.log(form.user)
+      var user_login = new User_factory(form.user)
       user_login.$authenticate(function(data){
             console.log('front:LoginUserController',data);
+            form.password.$error.wrongpassword = true;
+            $scope.errorMessage = data.message;
+            form.$invalid = true;
+            $scope.user.password = '';
             
 
       },function(err){
