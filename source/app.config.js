bd9edd61ebe4a29b8dae139bf24ba635f5339981
Glitch (crud-mongoose) <none>
2018-03-11 13:58:58 +0000

    👍🐥 Checkpoint source/app.config.js:531956/42 app/routes/users.js:531956/1

--- source/app.config.js
+++ source/app.config.js
@@ -1,4 +1,12 @@
 var CrudMongoose = angular.module('CrudMongoose', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router','ngResource','ngCookies'])
                           .config(['$httpProvider', function ($httpProvider) {
-                            
-                          }
+                            $httpProvider.interceptors.push(function($q, $cookies) {
+                              return {
+                               'request': function(config) {
+
+                                    config.headers['x-access-token'] = $cookies.token;
+                                    return config;
+                                }
+                              };
+                            });
+                          }])
