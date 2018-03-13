e484420d824fa9e6ccc80c9ba7da6e274abf56be
Glitch (crud-mongoose) <none>
2018-03-13 13:26:37 +0000

    🏁👌 Checkpoint source/components/register/route.js:531956/73 source/components/toolbar/toolbar.controller.js:531956/104 source/index.html:531956/42 source/components/toolbar/toolbar.html:531956/46 source/components/register/user.login.controller.js:531956/2

--- source/components/toolbar/toolbar.controller.js
+++ source/components/toolbar/toolbar.controller.js
@@ -1,7 +1,12 @@
 (function(app) {
-	app.controller('ToolbarController', ['$scope', function($scope) {
+	app.controller('ToolbarController', ['$rootScope','$scope', function($rootScope,$scope) {
     console.log('ToolbarController')
     
+    $scope.logout = function(){
+      console.log('ToolbarController: logout')
+      $rootScope.CrudMongooseGlobal.connecting = false
+      $rootScope.CrudMongooseGlobal.user = {}
+    }
     
 	}]);
 })(CrudMongoose);
