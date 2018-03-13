6e0a617b5bee704bcb8a6fbcda37fc66161b7181
Glitch (crud-mongoose) <none>
2018-03-13 17:48:34 +0000

    🌱💵 Checkpoint source/services/auth_services.js:531956/103 source/services/auth.service.js:531956/19

--- source/services/auth_services.js
+++ source/services/auth_services.js
@@ -2,16 +2,19 @@
 	app.factory('AuthServices', ['$q','User_factory','$cookies', function($q,$cookies,User_factory) {
       var auth ={};
     
-      auth.login=function(user,password){
+      auth.login=function(user){
         var deferred = $q.defer();
         
-        var user_login = new User_factory(user)
+        var userMongoDB = new User_factory(user)
       
-        user_login.$authenticate(function(data){
+        userMongoDB.$authenticate(function(data){
           if(data.success){      
-
-            // use RegisterService to centralize login state
-            AuthService.setAuthorized( $scope.user,data.token)
+            /* Add token in cookies client
+            Use by config.headers['x-access-token'] in request $http
+            instanciate to app.config.js
+            */
+            $cookies.put('token',data.token)
+            auth.user = user
           }
         }
         
