367dcf2072f8a3253cf211a14fca34a6929382f2
Glitch (crud-mongoose) <none>
2018-03-17 18:11:30 +0000

    🏫👨 Checkpoint ./app/routes/authenticate.js:531956/2934

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -1,10 +1,10 @@
 //================================== routes for API AUTHENTICATE ====================================
-
+const AUTHENTICATE_TOKEN_LAPS = 60*10 // expires in 10 minutes
 var express = require('express')
   , router = express.Router()
   ,	mongoose = require('mongoose')
   , jwt    = require('jsonwebtoken') // used to create, sign, and verify tokens
-
+  , bcrypt = require('bcrypt') // to hash/compare password
   ,	schema = require('../models/Person')
 
 
@@ -13,46 +13,36 @@ router.post('/authenticate', function(request, response) {
   
 
 	var User = mongoose.model('Users', schema )
-
-
-	User.findOne({email:request.body.email},function(err,user){
+  User.findOne({email:request.body.email},function(err,user){
 		if (err) throw err;
 
     if (!user) {
         res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
+    } else {
       // check if password matches
       // Load hash from your password DB.
       bcrypt.compare(request.body.password, user.password, function(err, res) {
         if (err) throw err;
-        if (!res) {
-          response.json({ success: false, message: 'Authentication failed. Wrong password.' });
-        }else{
+        if (res) {
+          var payload = {user: user.email};
+          var token = jwt.sign(payload, 
+                               'superSecret', 
+                               {  expiresIn: AUTHENTICATE_TOKEN_LAPS
+                                });
+
+          response.json({ success: true, message: 'Good your token JWT:',token:token });
           
-        }
+        }else{
+          response.json({ success: false, message: 'Authentication failed. Wrong password.' });
+        }          
         
         
       });
-      
-      if (user.password != req.body.password | !user.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-        const payload = {
-            user: user.email
-          };
-            var token = jwt.sign(payload, 'superSecret', {
-
-              expiresIn: 60*10 // expires in 10 minutes
-            });
-
-        res.json({ success: true, message: 'Good your token JWT.',token:token });	
-      }
-
-      
+    // End if/else section after find user
     }
-
-
+  // End findOne callback 
 	})
+// End authenticate post callback 
 })
 
 
