40474b7c416762e0718479a348772d11f44a8f83
Glitch (crud-mongoose) <none>
2018-03-21 14:28:09 +0000

    😻🐡 Checkpoint ./source/components/toolbar/toast.login.controller.js:531956/3381

--- source/components/toolbar/toast.login.controller.js
+++ source/components/toolbar/toast.login.controller.js
@@ -1,9 +1,21 @@
 (function(app) {
 	app.controller('LoginToastController', ['$scope', '$mdToast',function($scope,$mdToast {
   $scope.toastVisible =false;
-  
+  $scope.dismissAction="";
+                                                                         
   $scope.ShowLoginToast = function(){
     $scope.toastVisible =true;
+    var toast ={
+      controller : LoginToastController,
+      templateUrl: 'toast.login.html',
+      position: "top right",
+      hideDelay: 1000;
+      
+    };
+    
+    $mdToast.show(toast){
+      
+    }
     
   }
                                                                          
