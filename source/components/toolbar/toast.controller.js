2865c6c433be5f74ad132b40e182c98a6d618de4
Glitch (crud-mongoose) <none>
2018-03-21 19:21:39 +0000

    🦁👽 Checkpoint ./source/components/toolbar/toast.controller.js:531956/84

--- source/components/toolbar/toast.controller.js
+++ source/components/toolbar/toast.controller.js
@@ -1,13 +1,16 @@
 (function(app) {
 	app.controller('ToastController', ['$scope','AuthServices',function($scope,AuthServices) {
     $scope.authorized = undefined;
-      
+    $scope.toastVisible = false;
+    $scope.dismissAction = "";
+    
+    
     $scope.$watch(function(){
                     return AuthServices.user },
                   function(newAuthorized,oldAuthorized){
       
       $scope.authorized =  !(typeof newAuthorized === 'undefined')
-      
+      toastLogin();
       console.log('ToastController:$watch:authorized:'+$scope.authorized )
     })
     
