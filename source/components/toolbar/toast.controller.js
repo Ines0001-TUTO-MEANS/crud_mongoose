7793686fc83fdf55e2ed2fd25ac1fd9d5a9602e9
Glitch (crud-mongoose) <none>
2018-03-21 19:11:12 +0000

    🌘🚅 Checkpoint ./source/components/toolbar/toast.controller.js:531956/1874 ./source/components/toolbar/toolbar.controller.js:531956/529

--- source/components/toolbar/toast.controller.js
+++ source/components/toolbar/toast.controller.js
@@ -1,6 +1,34 @@
 (function(app) {
-	app.controller('ToastController', ['$scope',function($scope) {
+	app.controller('ToastController', ['$scope','AuthServices',function($scope,AuthServices) {
+    $scope.authorized = undefined;
+      
+    $scope.$watch(function(){
+                    return AuthServices.user },
+                  function(newAuthorized,oldAuthorized){
+      
+      $scope.authorized =  !(typeof newAuthorized === 'undefined')
+      
+      console.log('ToastController:$watch:authorized:'+$scope.authorized )
+    })
     
+    function toastLogin(){
+    
+      if($scope.authorized){
+          $scope.toastVisible =true;
+          var toast ={
+                controller : 'ToastController',
+                templateUrl: 'components/toolbar/toast.login.html',
+                position: "top right",
+                hideDelay: 5000
+              };
+
+          $mdToast.show(toast).then(function(response){
+            console.log('LoginToastController: timeout delay')
+            $scope.toastVisible = false;
+          })
+      }
+    
+    }
     
 	}]);
 })(CrudMongoose);
--- source/components/toolbar/toolbar.controller.js
+++ source/components/toolbar/toolbar.controller.js
@@ -1,8 +1,6 @@
 (function(app) {
 	app.controller('ToolbarController', ['$scope','$state','AuthServices','$mdToast',function($scope,$state,AuthServices,$mdToast) {
     $scope.authorized = false;
-    $scope.toastVisible =false;
-    $scope.dismissAction="";
     
     $scope.logout = function(){
       AuthServices.logout().then(function(resolve){
@@ -19,20 +17,7 @@
                   function(newAuthorized,oldAuthorized){
       
       $scope.authorized =  !(typeof newAuthorized === 'undefined')
-      if($scope.authorized){
-          $scope.toastVisible =true;
-          var toast ={
-                controller : 'ToastController',
-                templateUrl: 'components/toolbar/toast.login.html',
-                position: "top right",
-                hideDelay: 5000
-              };
-
-          $mdToast.show(toast).then(function(response){
-            console.log('LoginToastController: timeout delay')
-            $scope.toastVisible = false;
-          })
-      }
+      
       console.log('ToolbarController:$watch:authorized:'+$scope.authorized )
     })
     
