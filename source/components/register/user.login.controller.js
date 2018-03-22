1ce7e87c8600580516bd7212dbeb312e4fecf5b8
Glitch (crud-mongoose) <none>
2018-03-22 09:46:26 +0000

    🏡👂 Checkpoint ./source/components/register/user.login.html:531956/736 ./source/components/register/user.login.controller.js:531956/194

--- source/components/register/user.login.controller.js
+++ source/components/register/user.login.controller.js
@@ -2,15 +2,16 @@
 	app.controller('LoginUserController', ['$q','$scope', '$state','AuthServices',function($q,$scope,$state,AuthServices,LoginToastController) {
     $scope.user={};
     $scope.errorMessage = '';
-    $scope.activated = false;
+    $scope.showSpinner = false;
     $scope.imagePath = '/img/icons/nodejs.png';
     
     $scope.login = function(form){
-      
+      $scope.showSpinner = true;
       // Call authenticate service
       AuthServices.login($scope.user).then(function(data){
           // Switch consult users.list link
           $state.go('users.list',undefined,{reload:true})
+          $scope.showSpinner = false;
           
       
       },function(err){
@@ -18,6 +19,7 @@
           $scope.errorMessage = err;
           form.$invalid = true;
           $scope.user.password = '';
+          $scope.showSpinner = false;
       })
       
     }
