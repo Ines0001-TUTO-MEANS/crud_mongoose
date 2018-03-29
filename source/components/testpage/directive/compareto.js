e70cd05db645837cc3002e7a59c132fcbc2ebc24
Glitch (crud-mongoose) <none>
2018-03-29 15:39:36 +0000

    🚏🏙 Checkpoint ./source/components/testpage/directive/compareto.js:531956/1624

--- source/components/testpage/directive/compareto.js
+++ source/components/testpage/directive/compareto.js
@@ -1,23 +1,17 @@
 (function(app) {
 	
   
-  app.directive('compareto', function() {
+  app.directive('compareTo', function() {
    return {
       require:'^ngModel',
-      controller: function($scope){
-          var expanders = [];
-        
-          this.addExpander = function(expander){
-            expanders.push(expander);
-          }
-          
-          this.collapseAll = function(expander){
-            expanders.forEach(function(item){
-              if(item!=expander) item.ngtoggle = false
-            })
-          }
-      
-      
+      scope: {
+            compareTo: "="
+      },
+      link: function(scope,elem,attrbs,ngModelCtrl){
+          scope.$watch('compareTo',function(){
+            ngModelCtrl.
+          })    
+
       }
       
    } 
