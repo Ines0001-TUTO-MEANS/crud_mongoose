eabc3675efd9a1ad243930bab6e77f9014a3caf5
Glitch (crud-mongoose) <none>
2018-04-01 22:17:41 +0000

    💊🐦 Checkpoint ./source/components/testpage/directive/compareto.js:531956/1321

--- source/components/testpage/directive/compareto.js
+++ source/components/testpage/directive/compareto.js
@@ -1,14 +1,33 @@
 (function(app) {
 	
-  app.directive('compareTo', function() {
+  app.directive('compareTo',['$q','$timeout',function($q,$timeout) {
    return {
       require:'ngModel',
       scope: {
             compareTo: "="
       },
       link: function(scope,elem,attrbs,ngModel){
+          
+          /*
           ngModel.$validators.notmatch = function(modelValue) {
               return modelValue == scope.compareTo;
+          };
+          */
+        
+          ngModel.$asyncValidators.nomatch = function(modelvalue){
+            var def = $q.defer();
+     
+            $timeout(function() {
+                if (modelValue === scope.compareTo) {
+                    
+                    def.resolve();
+                } else {
+                    def.reject();
+                }
+            }, 1000);
+
+            return def.promise;
+            
           };
 
           scope.$watch("compareTo", function() {
@@ -20,6 +39,6 @@
       
    } 
     
-	});
+	}]);
   
 })(CrudMongoose);
