089ce338fd247e1095e9fd27cc1460282d4c4173
Glitch (crud-mongoose) <none>
2018-04-02 20:29:02 +0000

    🌄⭐️ Checkpoint ./source/components/register/auth.register.html:531956/12 ./source/components/register/user.auth.controller.js:531956/6000

--- source/components/register/user.auth.controller.js
+++ source/components/register/user.auth.controller.js
@@ -26,5 +26,31 @@
       
     }
     
+    $scope.register = function(form){
+      $scope.showSpinner = true;
+      form.$invalid = true;
+      // Call create new user
+      User_factory.save(form.user,function(data){
+             // success
+             // Call authenticate service
+            AuthServices.login($scope.user).then(function(data){
+                // Switch consult users.list link
+                $state.go('users.list',undefined,{reload:true})
+                $scope.showSpinner = false;
+
+
+            },function(err){
+
+                $scope.user= {};
+                $scope.showSpinner = false;
+            })
+          
+          },function(err){
+             // error
+             console.log(err);
+      })
+      
+    }
+    
 	}]);
 })(CrudMongoose);
