36e09f1dc6eb5643a0d8e53b63333094c64992a1
Glitch (crud-mongoose) <none>
2018-04-05 09:07:35 +0000

    🚎🍛 Checkpoint ./app/models/user.js:531956/3297

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose'),
     validate = require('mongoose-validator'),
-    Schema = mongoose.Schema;
-    
+    Schema = mongoose.Schema,
+    opt = {
+            // If set timestamps, mongoose assigns createdAt and updatedAt fields to your schema,
+            timestamps:true
 
-
+          };
 /**
  * @module  User
  * @description contain the details of Attribute
@@ -12,9 +14,24 @@ var mongoose = require('mongoose'),
 var User = new Schema({
 
     /** 
-      userName. It can only contain valid email id, should be unique, is required and indexed.
+      userName. It can only contain valid email id, should be unique, is required.
     */
     userName: {
+      type: String,
+      unique: true,
+      required :'email is mandatory',
+      validate: [
+          validate({
+            validator: 'isEmail',
+            message: 'email property is not valid Email',
+          })
+      ]
+    },
+  
+    /** 
+      userFirstname. It can only contain string, is required field.
+    */
+    userFirstname: {
         type: String,
         unique: true,
         required: true
@@ -28,58 +45,47 @@ var User = new Schema({
         required: true
     },
 
-    /** 
-    Scope. It can only contain string, is required field, and should have value from enum array.
-  */
-    scope: {
-        type: String,
-        enum: ['Customer'],
-        required: true
-    },
-
+   
     /** 
       isVerified. false default.
     */
     isVerified: {
         type: Boolean,
         default: false
-    }
+    },
     
     /** 
-      userName. It can only contain valid email id, should be unique, is required and indexed.
-    */
-    name: {
-        type: String,
-        required: 'name is mandatory'
-    },
+      Optionnal information. not required.
+    */                  
     age: {
       type: Number,
       min:[18, 'age limit 18']
     },
+    
     company: String,
-    email: {
-      type: String,
-      unique: true,
-      required :'email is mandatory',
-      validate: [
-          validate({
-            validator: 'isEmail',
-            message: 'email property is not valid Email',
-          })
-      ]
-    },
+    
+    
+    /** 
+      password. It can only contain string, is required field.
+    */  
     password: {
       type: String,
       required :'password is mandatory',
     },
+    
+      
+    /** 
+      admin. false by default.
+    */
     admin: {
       type: Boolean,
       message:'property is not a boolean value',
       default: false
     },
+    
     tasks : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tasks' }]
 
-});
+},opt);
 
 User.statics.saveUser = function(requestData, callback) {
     this.create(requestData, callback);
