e11ff10c4aa231340ca1a2517fb97489d3177bd8
Glitch (restfull-api) <none>
2018-04-11 09:59:32 +0000

    💠👕 Checkpoint ./app/controller/task/tasks.js:531956/826

---
+++ app/route/tasks.js
@@ -0,0 +1,58 @@
+var express = require('express')
+    ,router = express.Router()
+    ,mongoose = require('mongoose')
+    ,restify = require('express-restify-mongoose')
+    ,Boom = require('boom');
+
+var schema = require('../models/Task')
+
+// Using query builder
+
+
+
+var loginRequired (req, res, next) {
+  if (req.headers['x-access-token'] || req.body.token || req.query.token) {
+    next();
+  } else {
+    return next(Boom.forbidden('No token provided')); // HTTP 403
+  }
+};
+
+router.use(loginRequired);
+restify.serve(router, mongoose.model('Tasks', schema ))
+
+module.exports = router
+
+/*
+// route middleware to verify a token
+
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, 'superSecret', function(err, decoded) {      
+      if (err) {
+
+        next(Boom.unauthorized('The request has not been applied because it lacks valid authentication credentials for the target resource.'));
+         
+
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    next(Boom.forbidden('No token provided.'));    
+
+  }
+});
+*/
