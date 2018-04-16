0fbe2eeb6a6df658ce193788ed92663f9afcf96e
Glitch (restfull-api) <none>
2018-04-16 20:35:33 +0000

    🔑📍 Checkpoint ./source/services/boom.js:531956/758 ./source/components/testpage/boomCtrl.js:531956/31

--- source/components/testpage/boomCtrl.js
+++ source/components/testpage/boomCtrl.js
@@ -1,7 +1,7 @@
 (function(app) {
 	app.controller('boomCtrl', ['$scope','BoomService',function($scope,BoomService) {
     var boom = new BoomService({isBoom:true,output:{statusCode :'301'}});
-    console.log('boomCtrl',boom)
+    console.log('boomCtrl',boom.statusCode())
         
 	}]);
 })(CrudMongoose);
--- source/services/boom.js
+++ source/services/boom.js
@@ -1,21 +1,20 @@
 (function(app) {
-	app.service('BoomService', function() {
-      this.error ={}
-      
-      this.BoomService = function(error){
-         this.error = error; 
-      }
-    
-      this.statusCode = function(error){ 
+	app.factory('BoomService', function() {
+    function constructor(error){
+      //this.error = error
+
+      this.statusCode = function(){ 
           return error.isBoom ?error.output.statusCode:error;
       }
-      this.error = function(error) {
+      this.error = function() {
           return  error.isBoom ?error.output.payload.error:error
       };
       this.message = function(error) {
               return error.isBoom ?error.output.message:error
       }
-      
+    }
 
+    return constructor;
+    
 	});
 })(CrudMongoose);
