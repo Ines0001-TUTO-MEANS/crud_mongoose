c7d6eb36e6e542f049d85139b22d117a20c8cacc
Glitch (restfull-api) <none>
2018-04-16 18:57:58 +0000

    👲🍂 Checkpoint ./source/services/boom.js:531956/28

--- source/services/boom.js
+++ source/services/boom.js
@@ -1,17 +1,17 @@
 (function(app) {
 	app.service('BoomService', function(error) {
       function isBoom(){ return error.isBoom }
+      
       this.statusCode = get(error){ 
           return isBoom()?error.output.statusCode:error;
       }
       this.error = get(error) {
           return  isBoom()?error.output.payload.error:error
       };
-      this.message = get() {
+      this.message = get(error) {
               return isBoom()?error.output.message:error
-          }
-          
       }
+      
 
 	});
 })(CrudMongoose);
