e7212631907c88f848b55e1cb2e1fcbdaca03c56
Glitch (restfull-api) <none>
2018-04-18 13:18:20 +0000

    🚅🛢 Checkpoint ./app/route/user.js:531956/16

--- app/route/user.js
+++ app/route/user.js
@@ -10,6 +10,7 @@ router.post('/user', User.create);
 router.get('/verifyEmail/:token', User.verifyEmail);
 router.post('/resendVerificationEmail', User.resendVerificationEmail);
 router.post('/login', User.login);
+router.get('/')
 
 
 
