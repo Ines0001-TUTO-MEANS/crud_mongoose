f8e2911859e1ea2272bd8eb6bd0953cdd76b5524
Glitch (restfull-api) <none>
2018-04-18 20:39:21 +0000

    🔪🚣 Checkpoint ./source/public/forgotCtrl.js:531956/516

--- source/public/forgotCtrl.js
+++ source/public/forgotCtrl.js
@@ -1,8 +1,18 @@
 (function(app) {
-	app.controller('ForgotPasswordCtrl', ['$scope', function($scope) {
+	app.controller('ForgotPasswordCtrl', ['$scope', '$http', function($scope,$http) {
     
     $scope.user = { userName:'ines@gmail.com',password:'ines'};
-      
+    
+    $http({
+      method: 'GET',
+      url: '/someUrl'
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
     
 	}]);
 })(mApp);
